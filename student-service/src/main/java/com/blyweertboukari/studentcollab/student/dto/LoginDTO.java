8b0a4a68f2dfb3c0b7d348b9015f4146648a5657
Mateo Blyweert <mateo.bwe@gmail.com>
2025-11-19 15:22:18 +0100

    refactor: project properties

---
+++ student-service/src/main/java/com/blyweertboukari/studentcollab/student/dto/LoginDTO.java
@@ -0,0 +1,20 @@
+package com.blyweertboukari.studentcollab.student.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "L'email doit être valide")
+    private String email;
+    
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String password;
+}

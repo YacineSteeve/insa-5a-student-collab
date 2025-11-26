f4e656a7bc9d7342d2d6b09cf080c4937605912d
mateobwe <mateo.bwe@gmail.com>
2025-11-26 11:05:40 +0100

    fix: services mapping

--- help-request-service/src/main/java/com/blyweertboukari/studentcollab/helprequest/controller/HelpRequestController.java
+++ help-request-service/src/main/java/com/blyweertboukari/studentcollab/helprequest/controller/HelpRequestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/help-requests")
 public class HelpRequestController {
     
     @Autowired

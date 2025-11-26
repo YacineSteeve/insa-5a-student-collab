e9fecdfdd5504fc16197ade0635682cfd925e73f
Yacine BOUKARI <steeveboukari9@gmail.com>
2025-11-26 11:35:26 +0100

    refactor: student service controllers

--- help-request-service/src/main/java/com/blyweertboukari/studentcollab/helprequest/dto/StudentDTO.java
+++ help-request-service/src/main/java/com/blyweertboukari/studentcollab/helprequest/dto/StudentDTO.java
@@ -4,9 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentDTO {
-
+    private Long id;
+    private String nom;
+    private String email;
+    private String etablissement;
+    private String filiere;
+    private List<String> competences;
+    private List<String> disponibilites;
+    private Double moyenneAvis;
 }

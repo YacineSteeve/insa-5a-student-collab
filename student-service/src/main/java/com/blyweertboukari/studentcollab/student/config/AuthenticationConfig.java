fa8e992862cf131ed4ca2dd28c03181b619daf97
Yacine BOUKARI <steeveboukari9@gmail.com>
2025-11-26 15:10:32 +0100

    feat: add jwt authentication

---
+++ student-service/src/main/java/com/blyweertboukari/studentcollab/student/config/AuthenticationConfig.java
@@ -0,0 +1,45 @@
+package com.blyweertboukari.studentcollab.student.config;
+
+import com.blyweertboukari.studentcollab.student.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AuthenticationConfig {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return email -> studentRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Student not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+}
--- student-service/src/main/java/com/blyweertboukari/studentcollab/student/model/Student.java
+++ student-service/src/main/java/com/blyweertboukari/studentcollab/student/model/Student.java
@@ -4,45 +4,47 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "students")
+@Table(name = "student")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Student {
-    
+public class Student implements UserDetails  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false)
     private String lastName;
 
     @Column(nullable = false)
     private String firstName;
-    
+
     @Column(nullable = false, unique = true)
     private String email;
-    
+
     @Column(nullable = false)
     private String password;
-    
+
     @Column(nullable = false)
     private String establishment;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Major major;
-    
+
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "student_skills", joinColumns = @JoinColumn(name = "student_id"))
     @Column(name = "skills")
     private List<String> skills = new ArrayList<>();
-    
+
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "student_availabilities", joinColumns = @JoinColumn(name = "student_id"))
     @Column(name = "availabilities")
@@ -50,7 +52,7 @@ public class Student {
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews = new ArrayList<>();
-    
+
     @Transient
     public Double getAverageRating() {
         if (reviews == null || reviews.isEmpty()) {
@@ -60,6 +62,37 @@ public class Student {
                 .mapToInt(Review::getRating)
                 .average()
                 .orElse(0.0);
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public enum Major {

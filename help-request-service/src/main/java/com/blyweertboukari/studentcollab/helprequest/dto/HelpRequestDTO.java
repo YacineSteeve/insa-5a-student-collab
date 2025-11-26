b80b13824a7d4d7915665126cfd2122c5879b695
Yacine BOUKARI <steeveboukari9@gmail.com>
2025-11-26 19:46:52 +0100

    refactor: student controller

--- help-request-service/src/main/java/com/blyweertboukari/studentcollab/helprequest/dto/HelpRequestDTO.java
+++ help-request-service/src/main/java/com/blyweertboukari/studentcollab/helprequest/dto/HelpRequestDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -19,6 +20,6 @@ public class HelpRequestDTO {
     private String description;
     private List<String> keywords = new ArrayList<>();
     private Instant createdAt;
-    private Instant desiredDate;
+    private Date desiredDate;
     private HelpRequest.Status status;
 }

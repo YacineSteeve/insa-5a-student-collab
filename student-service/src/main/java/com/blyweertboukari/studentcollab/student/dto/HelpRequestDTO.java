209757d50f08d6d194a40b09226056c424cd8e38
Mateo Blyweert <mateo.bwe@gmail.com>
2025-11-27 15:58:37 +0100

    feat: reviews implementation

---
+++ student-service/src/main/java/com/blyweertboukari/studentcollab/student/dto/HelpRequestDTO.java
@@ -0,0 +1,31 @@
+package com.blyweertboukari.studentcollab.recommendation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelpRequestDTO {
+
+    private Long id;
+    private Long authorId;
+    private String title;
+    private String description;
+    private List<String> keywords = new ArrayList<>();
+    private Instant createdAt;
+    private Status status;
+
+    public enum Status {
+        WAITING,
+        IN_PROGRESS,
+        DONE,
+        ABANDONED,
+        CLOSED
+    }
+}
